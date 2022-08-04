440a601329a18776e136088a9f16252126f6eddf
khunemz <khunemz@gmail.com>
2022-08-04 23:32:24 +0700

    :test_tube: add fail test and command of cc

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,38 @@
-import { render, screen } from "@testing-library/react";
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from "./App";
+import mockFetch from "./mocks/mockFetch";
+
+
+beforeEach(() => {
+  jest.spyOn(window, "fetch").mockImplementation(mockFetch);
+})
+
+
+afterEach(() => {
+  jest.restoreAllMocks()
+});
 
 test("renders the landing page", () => {
   render(<App />);
+
+  expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
+  expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
+  expect(screen.getByRole("button", { name: "Search" })).toBeDisabled();
+  expect(screen.getByRole("img")).toBeInTheDocument();
 });
+
+
+test("should be able to search and display dog image results", async () => {
+  render(<App />);
+   
+  //...Simulate selecting an option and verifying its value
+
+ //Simulate initiating the search request
+  const searchBtn = screen.getByRole("button", { name: "Search" });
+  expect(searchBtn).not.toBeDisabled();
+  userEvent.click(searchBtn);
+
+  //Loading state displays and gets removed once results are displayed
+  await waitForElementToBeRemoved(() => screen.queryByText(/Loading/i));
+})
